6239ef1c6168b2cd949397396135282b5975184e
Gustavo Xavier <gxavier8477@gmail.com>
2024-11-18 20:27:28 -0300

    Ajustes pequenos JS
    Pequenas correções

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 
 /* Pfv, nn esquecam de colocar o ID idêntico ao do JS */
 
-const track = document.getElementById(carrosselImagens);
+const track = document.getElementById('carrosselImagens');
 
-const itens = document.querySelectorAll(carrosselItens);
+const items = document.querySelectorAll('.section1');
 let index = 0;
 
 /* Função que vai passar as próximas imagens */
 function animacaoCarrossel() {
     index++; /* Elemento usado para avançar uma imagem */
 
-    if (index >= itens.length - 1) {
-        setTimeout(() => {
+    if (index >= itens.length -1) {
+        setTimeout(() =>{
             track.style.transition = 'none'; /* Removendo animação */
             index = 0; /* Voltando a primeira imagem */
             track.style.transform = `translateX(0)`; /* Movendo para a primeira imagem */
-        }, 500); /* Tempo suficiente para completar a  */
+        }, 500); /* Tempo suficiente para completar a volta */
     }
     else {
         track.style.transition = 'transform 1.0 ease';
